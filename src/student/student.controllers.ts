64a083910a7e9cc596f7d30acb632b3a841ab7e7
eren kiraz <erengkiraz@gmail.com>
2021-12-09 12:48:01 +0300

    controllers added.

---
+++ src/student/student.controllers.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Post, Put } from "@nestjs/common"
+
+
+@Controller('students')
+export class StudentController {
+
+    @Get()
+    getStudents() {
+        return "All Students"
+    }
+
+    @Get('/:studentId')
+    getStudentById(
+        // @Req() req: Request
+    ) {
+        // return req;
+        return "Get Student By Id"
+    }
+
+    @Post()
+    createStudent() {
+        return "Create Student"
+    }
+
+    @Put('/:studentId')
+    updateStudent() {
+        return "Update Student By Id"
+    }
+}
