b182aa6d65e8abe18513f1da95ba2f14d0f41921
eren kiraz <erengkiraz@gmail.com>
2021-12-09 15:13:54 +0300

    data transfer objects added.

--- src/teacher/student.controller.ts
+++ src/teacher/student.controller.ts
@@ -1,13 +1,14 @@
 
    
 import { Controller, Get, Param, Put } from '@nestjs/common';
+import { StudentResponseDto } from 'src/student/dto/student.dto';
 
 @Controller('teachers/:teacherId/students')
 export class StudentTeacherController {
     @Get()
     getStudents(
         @Param('teacherId') teacherId: string 
-    ) {
+    ): FindTeacherResponseDto[] {
        return `Get All Students That Belong To A Teacher With Id of ${teacherId}`
     }
 
@@ -15,7 +16,7 @@ export class StudentTeacherController {
     updateStudentTeacher(
         @Param('teacherId') teacherId: string,
         @Param('studentId') studentId: string
-    ) {
+    ): StudentResponseDto {
         return `Update Student With Id Of ${studentId} To Teacher With Id Of ${teacherId}`
     }
 }
